2f3d9c53b22854b55177af13ec66ea8cfaba9ae0
IsaacYam <howingyam0350@gmail.com>
2025-12-03 21:06:32 +0800

    update

--- medusa-config.ts
+++ medusa-config.ts
@@ -1,6 +1,6 @@
-import { loadEnv, defineConfig } from '@medusajs/framework/utils'
+import { loadEnv, defineConfig } from "@medusajs/framework/utils"
 
-loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+loadEnv(process.env.NODE_ENV || "development", process.cwd())
 
 module.exports = defineConfig({
   projectConfig: {
@@ -12,6 +12,29 @@ module.exports = defineConfig({
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
-  }
+    },
+  },
+
+  modules: [
+    {
+      resolve: "@medusajs/medusa/file",
+      options: {
+        providers: [
+          {
+            resolve: "@medusajs/medusa/file-s3",
+            id: "s3",
+            options: {
+              file_url: process.env.FILE_URL,
+              access_key_id: process.env.ACCESS_KEY_ID,
+              secret_access_key: process.env.SECRET_ACCESS_KEY,
+              region: process.env.REGION,
+              bucket: process.env.BUCKET,
+              endpoint: process.env.ENDPOINT,
+              // optional: additional_client_config if Railway needs it
+            },
+          },
+        ],
+      },
+    },
+  ],
 })
